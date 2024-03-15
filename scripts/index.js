5f453cfaa16159bc033c8416c5160792380a7ebb
dudirhd <dmitriy-ermolenko-2002@mail.ru>
2024-03-15 13:51:28 +0500

    try

--- scripts/index.js
+++ scripts/index.js
@@ -5,19 +5,17 @@ const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function createCard(item) {
+function createCard(item, deleteCardFunction) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
   const cardImage = cardElement.querySelector('.card__image');
   const deleteButton = cardElement.querySelector('.card__delete-button');
   const cardTitle = cardElement.querySelector('.card__title');
 
-  deleteButton.addEventListener('click', function() {
-    const listItem = deleteButton.closest('.card');
-    listItem.remove();
-  });
+  deleteButton.addEventListener('click', deleteCardFunction);
 
   cardImage.src = item.link;
+  cardImage.alt = item.link;
   cardTitle.textContent = item.name;
  
   return cardElement;
@@ -25,9 +23,14 @@ function createCard(item) {
 
 // @todo: Функция удаления карточки
 
+function deleteCardFunction(cardElement) {
+  const itemRemove = cardElement.target.closest('.card');
+  itemRemove.remove();
+}
+
 // @todo: Вывести карточки на страницу
 
 initialCards.forEach(item => {
-  const card = createCard(item);
+  const card = createCard(item, deleteCardFunction);
   placesList.append(card);
 })
